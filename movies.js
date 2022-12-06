d3a5b4157566df4b5a870dfc230284ec42059701
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-12-06 15:31:06 -0600

    corrected some merge conflicts

--- movies.js
+++ movies.js
@@ -1,35 +1,53 @@
 // --------------------- Add Movie --------------------------- //
 // Adds a movie to the Glitch movie database.
+// function addMovie(title, year, genre, plot, rated) {
+//     $.post('https://determined-unleashed-ixia.glitch.me/movies', {
+//         title, year, genre, plot, rated
+//     }).done(function() {
+//         console.log('Movie added');
+//     });
+// }
+
+let movieAddBtn = document.querySelector('#movie-add-btn');
+movieAddBtn.addEventListener('click', addMovie);
+// function makes post request to glitch db
 function addMovie(title, year, genre, plot, rated) {
+    event.preventDefault();
+
+    let newTitle = document.querySelector('#new-movie-title').value
+    let newYear = document.querySelector('#new-movie-year').value
+    let newGenre = document.querySelector('#new-movie-genre').value
+    let newPlot = document.querySelector('#new-movie-plot').value
+    let newRated = document.querySelector('#new-movie-rated').value
+    let newPoster = document.querySelector('#new-movie-poster').value
+
     $.post('https://determined-unleashed-ixia.glitch.me/movies', {
         title, year, genre, plot, rated
     }).done(function() {
+        console.log(newTitle, newYear, newGenre, newPlot, newRated);
         console.log('Movie added');
     });
+
+    console.log(getMovies());
 }
-// addMovie("Spirited Away", "2008", "Animation, Adventure, Comedy", "A five-year-old boy develops a relationship with Ponyo, a young goldfish princess who longs to become a human after falling in love with him.", "G");
 
 // --------------------- Glitch Database --------------------------- //
 // Fetches movie information from the Glitch database.
 // Sets a "Loading" message while the promise is still pending.
 // getMovies function returns a list of movies in the Glitch database.
-
 fetch('https://determined-unleashed-ixia.glitch.me/movies')
     .then(response => response.json())
     .then(data => {
         setTimeout(() => {
             console.log('Glitch:', data);
             document.querySelector(".preload").style.display = "none"; // stop the load
-            
             document.querySelector(".display").style.display = "flex"; // show the main
-
         }, 3000);
     })
 
 function getMovies() {
     return fetch('https://determined-unleashed-ixia.glitch.me/movies').then(response => response.json());
 }
-
 getMovies().then(data => console.log('Glitch:', data[0].Title));
 
 // --------------------- Remove Movie Card --------------------------- //
@@ -53,12 +71,10 @@ function movieData(title) {
         console.log(`${title}:`, data);
     });
 }
-// movieData("little mermaid");
 
 function omdbData() {
     return fetch(omdbKey).then(response => response.json());
 }
-
 // omdbData().then(data => console.log('OMDB:', data));
 
 // --------------------- Create Movie Card --------------------------- //
@@ -82,13 +98,13 @@ function makeMovieCards() {
             console.log(Title, Year, Genre, Rated, Plot, Poster);
 
             // Generates Card HTML
-            movieCard += `<div class="card text-white bg-primary mb-3" style="width: 500px">`;
+            movieCard += `<div class="card text-white bg-primary mb-3">`;
             movieCard += `<div class="row g-0"><div class="col-md-4 d-flex">`;
             movieCard += `<img src="${Poster}" class="rounded-start img-fluid"></div>`;
             movieCard += `<div class="col-md-8"><div class="card-body">`;
             movieCard += `<h5 class="card-title d-inline">${Title}</h5>`;
             movieCard += `<small class="text-muted ms-2">${Year}</small>`;
-            movieCard += `<p class="card-text">${Plot}</p>`;
+            movieCard += `<p class="card-text mt-2">${Plot}</p>`;
             movieCard += `<p class="card-text">`;
             movieCard += `<small class="text-muted bottom">${Rated} | ${Genre}</small>`;
             movieCard += `</p></div></div></div></div>`;
@@ -104,36 +120,6 @@ function makeMovieCards() {
 }
 makeMovieCards();
 
-    // $.get("https://determined-unleashed-ixia.glitch.me/movies").done(function (data) {
-    //
-    //     omdbData().then(data => {
-            // console.log('OMDB:', data);
-
-            // // OMDB Data
-            // let {Title, Year, Genre, Rated, Plot, Poster} = data;
-            // // console.log(Title, Year, Genre, Rated, Plot, Poster);
-            //
-            // // Generates Card HTML
-            // let movieProfile = `<div class="card text-white bg-primary mb-3" style="width: 520px">`;
-            // movieProfile += `<div class="row g-0"><div class="col-md-4">`;
-            // movieProfile += `<img src="${Poster}" class="rounded-start img-fluid"></div>`;
-            // movieProfile += `<div class="col-md-8"><div class="card-body">`;
-            // movieProfile += `<h5 class="card-title">${Title}</h5>`;
-            // movieProfile += `<p class="card-text">${Plot}</p>`;
-            // movieProfile += `<p class="card-text">`;
-            // movieProfile += `<small class="text-muted">${Year} | </small>`;
-            // movieProfile += `<small class="text-muted">${Rated} | </small>`;
-            // movieProfile += `<small class="text-muted">${Genre}</small>`;
-            // movieProfile += `</p></div></div></div></div>`;
-            //
-            // let searchMovieContainer = document.querySelector('#movie-search-container');
-            // searchMovieContainer.innerHTML = movieProfile;
-
-//         });
-//     });
-// }
-// makeMovieCards();
-
 // --------------------- Movie Search --------------------------- //
 //
 let userMovieSearch = document.querySelector('#movie-search-btn');
@@ -148,64 +134,24 @@ function searchMovies() {
     fetch(omdbKey2).then((response) => {
         console.log('OMDB:', response.json());
     });
-})
-// --------------------- End Movie Search ----------------------------------
-$.get("https://determined-unleashed-ixia.glitch.me/movies").done(function (data) {
-    omdbData().then( data => {
-        console.log('OMDB:', data);
-
-        // Details from OMDB
-        let {Title, Year, Genre, Rated, Plot, Poster} = data;
-        // console.log(Title, Year, Genre, Rated, Plot, Poster);
-
-        // Pushes to the Card
-        $('#title').html(Title);
-        $('#year').html(Year);
-        $('#genre').html(Genre);
-        $('#rated').html(Rated);
-        $('#plot').html(Plot);
-        $('#img').html(`<img src="${Poster}" class="rounded-start img-fluid">`);
-
-    });
-});
-//------------ Add a Movie Function ----------------
-let movieAddBtn = document.querySelector('#movie-add-btn');
-movieAddBtn.addEventListener('click', addMovie);
-// function makes post request to glitch db
-function addMovie(title, year, genre, plot, rated) {
-    event.preventDefault();
-
-    let newTitle = document.querySelector('#new-movie-title').value
-    let newYear = document.querySelector('#new-movie-year').value
-    let newGenre = document.querySelector('#new-movie-genre').value
-    let newPlot = document.querySelector('#new-movie-plot').value
-    let newRated = document.querySelector('#new-movie-rated').value
-    let newPoster = document.querySelector('#new-movie-poster').value
-
-    $.post('https://determined-unleashed-ixia.glitch.me/movies', {
-        title, year, genre, plot, rated
-    }).done(function() {
-        console.log(newTitle, newYear, newGenre, newPlot, newRated);
-        console.log('Movie added');
-    });
-
-    console.log(getMovies());
-
 }
 
-//------------- Add Movie End -----------------------
+function searchData() {
+    return fetch(omdbKey2).then(response => response.json());
+}
+
 // Specifications
 // Your application should:
 //
 //      On page load:
-//      Completed: Display a "loading..." message
-//      Completed: Make an AJAX request to get a listing of all the movies
-//      Completed: When the initial AJAX request comes back, remove the "loading..." message and
-//      replace it with HTML generated from the json response your code receives
+//      Display a "loading..." message
+//      Make an AJAX request to get a listing of all the movies
+//      When the initial AJAX request comes back, remove the "loading..." message and
+//       replace it with HTML generated from the json response your code receives
 //
 //      Allow users to add new movies:
-//      TODO: Create a form for adding a new movie that has fields for the movie's title and           rating
-//      TODO: When the form is submitted, the page should not reload / refresh, instead, your           javascript
+//      TODO: Create a form for adding a new movie that has fields for the movie's title and rating
+//      TODO: When the form is submitted, the page should not reload / refresh, instead, your javascript
 //       should make a POST request to /movies with the information the user put into the form
 //
 //      Allow users to edit existing movies:
